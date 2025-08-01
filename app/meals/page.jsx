f89e1c6e0d3b878f4064ea40e108731efcafb3ae
strahinja800 <strahinjaa20@gmail.com>
2025-08-01 22:59:52 +0200

    Finished meals page

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,14 +1,28 @@
 import Link from 'next/link'
+import classes from './page.module.css'
+import MealsGrid from '@/components/meals/meals-grid'
+import { getMeals } from '@/lib/meals'
 
-export default function MealsPage() {
+export default async function MealsPage() {
+  const meals = await getMeals()
+
   return (
-    <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Welcome to the Meals Page!
-      </h1>
-      <p>
-        <Link href='../'>Home</Link>
-      </p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals created{' '}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favourite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href='/meals/share'>Share your favourite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={meals} />
+      </main>
+    </>
   )
 }
